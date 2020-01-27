e45a7ddc970c54c6e9f84392926d118de17ac2a7
Mathieu Lagrange <mathieu.lagrange@cnrs.fr>
2020-01-27 11:52:59 +0100

    inc

--- makeGit.sh
+++ makeGit.sh
@@ -2,7 +2,7 @@
 cd ~/papers/hdrThesis
 git pull
 cd -
-cp -r *tex *bib *bst *sh figures exp ~/papers/hdrThesis
+cp -r *tex *bib bib/*bib *bst *sh figures ~/papers/hdrThesis
 cd ~/papers/hdrThesis
 git add *tex *bib *bst *sh
 git add figures/* exp/*tex
